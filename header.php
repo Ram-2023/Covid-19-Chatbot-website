431224d2b5caf2900f7c7a4591d2c9540c27139c
Ram-2023 <60595141+Ram-2023@users.noreply.github.com>
2021-07-25 13:20:03 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Title</title>
+<style>
+body{
+	background-color:Aquamarine;
+	text-align:center;
+}
+h1{
+	color:Purple;
+	font-family:Monaco;
+}
+</style>
+</head>
+<body>
+<h1>COVID 19 CHATBOT</h1>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,6 @@
+<html>
+<frameset rows='10%,80%'>
+	<frame noresize src="header.php" frameborder=0>
+	<frame noresize src="index.php" frameborder=0>
+</frameset>
+</html>
